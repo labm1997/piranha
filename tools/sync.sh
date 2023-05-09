78918617da000f7d0517f3dea8b8b409a6d6873f
Luiz Antônio Borges Martins <labm1997@gmail.com>
2023-05-09 19:14:06 -0300

    Added new models

--- tools/sync.sh
+++ tools/sync.sh
@@ -15,4 +15,32 @@ echo "$CHECK Sending sync patch to remote server..."
 docker exec fedora /bin/scp -P10022 /mnt/Mestrado/Pesquisa/piranha/tools/sync.patch gabarito@localhost:~/piranha/sync.patch
 
 echo "$CHECK Applying patch on remote server..."
-docker exec fedora /bin/ssh gabarito@localhost -p10022 'cd piranha && git stash && git apply sync.patch'
+docker exec fedora /bin/ssh gabarito@localhost -p10022 'cd piranha && git stash && git apply sync.patch || true'
+
+# Sync extra files if option is passed
+if [ "$1" = "--files" ]; then
+    # Files to sync after everything
+    declare -a FILES=(
+        "./files/COVID/test_data"
+        "./files/COVID/test_labels"
+        "./files/COVID/train_data"
+        "./files/COVID/train_labels"
+    )
+
+    # Sync these files
+    for file in ${FILES[@]}; do
+        echo "$CHECK Syncing $file to remote server..."
+        
+        local_checksum=$(shasum $file | awk '{print $1}')
+        remote_checksum=$(docker exec fedora /bin/ssh gabarito@localhost -p10022 "shasum ~/piranha/$file | awk '{print \$1}'")
+
+        if [ "$local_checksum" != "$remote_checksum" ]; then
+            # Create folder
+            filedir=$(dirname $file)
+            docker exec fedora /bin/ssh gabarito@localhost -p10022 "mkdir -p ~/piranha/$filedir"
+
+            # Upload
+            docker exec fedora /bin/scp -P10022 /mnt/Mestrado/Pesquisa/piranha/$file gabarito@localhost:~/piranha/$file
+        fi
+    done
+fi
